aab3e09bdae7e4205d1d564774d598d4efe0af17
Goldy <goldypasulu19@gmai.com>
2024-06-18 16:35:43 +0700

    adding import_3_7

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -2,16 +2,37 @@
 
 namespace App\Imports;
 
+use App\Models\Product;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Illuminate\Support\Facades\Validator;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class ProductImport implements ToCollection
+class ProductImport implements ToCollection, WithHeadingRow
 {
     /**
     * @param Collection $collection
     */
-    public function collection(Collection $collection)
+    public function collection(Collection $rows)
     {
-        //
+        foreach ($rows as $row) {
+            $validator = Validator::make($row->toArray(), [
+                'name' => 'required',
+                'description' => 'required',
+                'price' => 'required|numeric',
+                'category_id' => 'required',
+                'branch_id' => 'required',
+                'status' => 'required|boolean',
+                'is_favorite' => 'required|boolean',
+                'stock' => 'required|numeric',
+            ]);
+
+            if ($validator->fails()) {
+                // Log errors or handle them as needed
+                continue;
+            }
+
+            Product::create($row->toArray());
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,8 @@ Route::middleware(['auth'])->group(function () {
 
     Route::resource('users', UserController::class);
     Route::resource('products', ProductController::class);
-    Route::get('products/import', [ProductController::class, 'import'])->name('products.import');
+    Route::get('products/import', [ProductController::class, 'importForm'])->name('products.importForm');
+    Route::post('products/import', [ProductController::class, 'import'])->name('products.import');
     Route::resource('other_product', OtherProductController::class);
     Route::resource('categories', CategoryController::class);
     Route::resource('customers', CustomerController::class);
