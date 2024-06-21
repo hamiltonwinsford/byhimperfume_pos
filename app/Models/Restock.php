bc354e56ed8524783598b2c17fdc2ae1947ae59f
Goldy <goldypasulu19@gmai.com>
2024-06-21 16:49:07 +0700

    Refactor Restock model to use guarded property and specify table name and primary key

--- app/Models/Restock.php
+++ app/Models/Restock.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Restock extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+    protected $primaryKey = 'id';
+    protected $table   = "restocks";
+    public $timestamps = false;
 }
