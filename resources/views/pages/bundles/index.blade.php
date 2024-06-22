decb11d26e2efe52c928a691bfa6453e11f4b4c0
Goldy <goldypasulu19@gmai.com>
2024-06-22 21:17:13 +0700

    Refactor bundle creation link in index.blade.php to use correct route name

--- resources/views/pages/bundles/index.blade.php
+++ resources/views/pages/bundles/index.blade.php
@@ -13,7 +13,7 @@
         <div class="section-header">
             <h1>Promotions</h1>
             <div class="section-header-button">
-                <a href="{{ route('promotionBundle.create') }}" class="btn btn-primary">Add New</a>
+                <a href="{{ route('bundle.create') }}" class="btn btn-primary">Add New</a>
             </div>
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
